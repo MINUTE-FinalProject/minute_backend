b0350b3071d3f595f8a21e18849075bc2dcb8046
bbwansu <orm6150@gmail.com>
2025-05-24 18:19:04 +0900

    #11 0524 17:24 공지사항 작성,수정, 삭제 postman 테스트 필요, 로그인 브랜치와 병합전으로 리셋

--- src/main/java/com/minute/board/notice/repository/NoticeRepository.java
+++ src/main/java/com/minute/board/notice/repository/NoticeRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor; // <<< 
 public interface NoticeRepository extends JpaRepository<Notice, Integer>, JpaSpecificationExecutor<Notice> {
     // Notice 엔티티의 ID (noticeId) 타입은 Integer 입니다.
     // 기능 구현 시 필요한 쿼리 메서드를 여기에 추가합니다.
+    //
 }
