9550852313942364b098ddceb3cd7e753736a8bc
yein1816 <ehowldpdls@gmail.com>
2025-05-26 00:26:14 +0900

    #12 0526 25:16 검색 최근검색어 삭제 기능 수정

--- src/main/java/com/minute/video/repository/SearchHistoryRepository.java
+++ src/main/java/com/minute/video/repository/SearchHistoryRepository.java
@@ -3,7 +3,9 @@ package com.minute.video.repository;
 import com.minute.user.entity.User;
 import com.minute.video.Entity.SearchHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,5 +15,8 @@ public interface SearchHistoryRepository extends JpaRepository<SearchHistory, In
     List<SearchHistory> findByUserUserIdOrderBySearchedAtDesc(String userId);
 
     // 최근 검색어 삭제
+    @Modifying
+    @Transactional
     void deleteByUserUserIdAndKeyword(String userId, String keyword);
+
 }
