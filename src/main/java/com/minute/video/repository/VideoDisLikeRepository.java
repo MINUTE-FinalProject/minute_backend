2c15642cb5bc94671996b48485801c89173fcb3d
hyunuk-gh <qkrgusdnr13@gmail.com>
2025-06-04 09:24:15 +0900

    0604 09:23 비디오 저장소,서비스 수정

--- src/main/java/com/minute/video/repository/VideoDisLikeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.minute.video.repository;
-
-import com.minute.video.Entity.VideoDislike;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface VideoDisLikeRepository extends JpaRepository<VideoDislike, Long>{
-    boolean existsByUserUserIdAndVideoVideoId(String userId, String videoId);
-    void deleteByUserUserIdAndVideoVideoId(String userId, String videoId);
-}
